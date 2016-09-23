b00067633a2beace37b0e22018b135274364b612
Jiří Mašek <Jiri.Masek2@firma.seznam.cz>
2016-09-23 07:37:59 +0200

    Inicializace projektu

---
+++ frpc-client/src/main/java/cz/sklik/frpc/App.java
@@ -0,0 +1,13 @@
+package cz.sklik.frpc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
